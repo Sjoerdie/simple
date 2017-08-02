52a2ddbf41b42b88b9bace30150916b042d5f508
Sjoerd Dijkstra <sjoerd_d60@hotmail.com>
2017-08-02 13:41:35 +0200

    Another try

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -42,7 +42,7 @@ function fish_prompt -d "Simple Fish Prompt"
     __print_color 5DAE8B "$pwd_string"
 
     if set -q VIRTUAL_ENV
-        set -l version (python --version ^| tr '\n' ' ' | cut -c 8-)
+        set -l version (python --version ^| tr '\n' ' ' | perl -pe '($_)=/([0-9]+([.][0-9]+)+)/')
         set -l virtual_env (basename "$VIRTUAL_ENV")
         __print_color ffffff " with "
         __print_color cc7832 "$virtual_env($version)"
