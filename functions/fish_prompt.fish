cf60d0209d79213c6bd073464ffb18c9b2c7b8f9
Sjoerd Dijkstra <sjoerd_d60@hotmail.com>
2017-08-02 13:21:38 +0200

    Correct order for older pythons

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -42,7 +42,7 @@ function fish_prompt -d "Simple Fish Prompt"
     __print_color 5DAE8B "$pwd_string"
 
     if set -q VIRTUAL_ENV
-        set -l version (python --version | cut -c 8- ^| tr '\n' ' ')
+        set -l version (python --version ^| tr '\n' ' ' | cut -c 8-)
         set -l virtual_env (basename "$VIRTUAL_ENV")
         __print_color ffffff " with "
         __print_color cc7832 "$virtual_env($version)"
